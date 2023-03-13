d389908ead47712daa4ea7b5cb820ba612295bcd
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-03-13 16:38:04 -0300

    fix size of the input on newsletter

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,4 +1,4 @@
-import { Container, Flex, Box, Input, Button } from "@chakra-ui/react"
+import { Container, Flex, Input, Button } from "@chakra-ui/react"
 import { SectionTitle } from "./SectionTitle"
 import bg from "../assets/news.jpg"
 
@@ -14,7 +14,7 @@ export const Subscribe = () => {
         >
             <Container maxW="3xl">
                 <SectionTitle title="NEWSLETTER" subtitle="Get the latest information from us" />
-                <Flex mx={[10, 20, 20]} mb={20} mt={18} bg="white" h="48px" borderRadius="lg" alignItems="center">
+                <Flex mx={[1, 20, 20]} mb={20} mt={18} bg="white" h="48px" borderRadius="lg" alignItems="center">
                     <Input variant='ghost' placeholder='youemail@lorem.com' h="full" w="full" color="black" />
                     <Button colorScheme='green' size='md' variant="solid" mr="4.5px" px={8}>
                         Subscribe
