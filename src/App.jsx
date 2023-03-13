6fc3d4a32abe974b75085749ab0e929d26557bc8
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-03-13 18:37:12 -0300

    add the gallery section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Plans } from "./components/Plans"
 import { Testimonials } from "./components/Testimonials"
 import { Subscribe } from "./components/Subscribe"
 import { Footer } from "./components/Footer"
-import { Galery } from "./components/Galery"
+import { Gallery } from "./components/Gallery"
 
 export const App = () => {
 
@@ -17,7 +17,7 @@ export const App = () => {
       <Testimonials />
       <Plans />
       <Subscribe />
-      <Galery />
+      <Gallery />
       <Footer />
     </div>
   )
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -8,11 +8,11 @@ import img4 from "../assets/galery4.jpg"
 
 const imgs = [{ img: img1, id: 1 }, { img: img2, id: 2 }, { img: img3, id: 3 }, { img: img4, id: 4 }]
 
-export const Galery = () => {
+export const Gallery = () => {
     return (
         <Flex py={5}>
             <Container maxW='4xl' display="flex" flexDir="COLUMN" justifyContent="center" pb={8}>
-                <SectionTitle title="GALERY" subtitle="see some pictures of our environments" />
+                <SectionTitle title="GALLERY" subtitle="see some pictures of our environments" />
                 <SimpleGrid columns={[1, 2, 2]} spacing={5}>
                     {imgs.map((img) => (
                         <Image src={img.img} key={img.id} ></Image>
